530f48434f43de9e907ccb51878a4b1cc2802bc1
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2024-08-16 15:12:21 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+OWNER = int(os.environ.get("OWNER", "7040444713"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "LCUFILE_STORE_BOT")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002208570147"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://me:me>@rename.axsbr.mongodb.net/?retryWrites=true&w=majority&appName=rename")
+DB_NAME = os.environ.get("DB_NAME", "rename")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "STRING")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 
 
