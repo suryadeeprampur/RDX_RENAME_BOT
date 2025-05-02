7dbbb22b176ac1af62dc164906fffec1d89a7756
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-05-02 16:16:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", "7040444713"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "LCUFILE_STORE_BOT")
+OWNER = int(os.environ.get("OWNER", "6004928770"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Rename4gbmv_Bot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002208570147"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001627302224"))
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://me:me>@rename.axsbr.mongodb.net/?retryWrites=true&w=majority&appName=rename")
-DB_NAME = os.environ.get("DB_NAME", "rename")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://rename4gbmv:rename4gbmv@cluster0.byymegh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 STRING = os.environ.get("STRING", "STRING")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://envs.sh/1y-.jpg")
 
 
 
