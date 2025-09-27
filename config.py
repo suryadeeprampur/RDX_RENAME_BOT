babd796e4bc414dea114d8ea90039be2437221f8
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-09-27 10:30:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", "6004928770"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "Rename4gbmv_Bot")
+OWNER = int(os.environ.get("OWNER", ""))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001627302224"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://rename4gbmv:rename4gbmv@cluster0.byymegh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://MovieClub:MovieClub@cluster0.dau2bnj.mongodb.net/MovieClub?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "MovieClub")
 
 STRING = os.environ.get("STRING", "STRING")
 BOT_PIC = os.environ.get("BOT_PIC", "https://envs.sh/1y-.jpg")
