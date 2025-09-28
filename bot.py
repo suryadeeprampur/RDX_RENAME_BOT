f23a8d5395e5fc3fb744fd9be7da131b689289e0
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-09-28 19:42:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, idle
-from plugins.cb_data import app as Client2
 from config import *
 import pyrogram.utils
 import asyncio
@@ -7,17 +6,19 @@ from aiohttp import web
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
+# -------------------------------
+# Main Bot Client
+# -------------------------------
 bot = Client(
-    "Renamer",
+    "RenamerBot",
     bot_token=BOT_TOKEN,
     api_id=API_ID,
     api_hash=API_HASH,
     plugins=dict(root="plugins")
 )
 
-
 # -------------------------------
-# Web server (for Koyeb/Heroku)
+# Web server (optional, for health checks)
 # -------------------------------
 async def web_server():
     async def handle(request):
@@ -27,33 +28,17 @@ async def web_server():
     app.router.add_get("/", handle)
     return app
 
-
 async def start_web():
     runner = web.AppRunner(await web_server())
     await runner.setup()
     await web.TCPSite(runner, "0.0.0.0", PORT).start()
 
-
-if STRING:
-    apps = [Client2, bot]
-    for app in apps:
-        app.start()
-    asyncio.get_event_loop().run_until_complete(start_web())
+# -------------------------------
+# Run Bot
+# -------------------------------
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_web())
+    bot.start()
     idle()
-    for app in apps:
-        app.stop()
-else:
-    bot.run()
-
-# Jishu Developer
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
+    bot.stop()
